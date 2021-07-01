54c385d0232e6d543a41635bff80f531222addec
Allan Hardy <al_hardy@live.com.au>
2021-07-01 15:56:05 +1000

    updating vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -8,10 +8,9 @@ locals {
 
 module "aws-vpc" {
   source                        = "app.terraform.io/bytebox/aws-vpc/module"
-  version                       = "0.0.1"
+  version                       = "0.0.2"
   aws_region                    = local.aws_region
   vpc_cidr                      = var.vpc_cidr
-  enable_vpc_flow_log           = false # TODO: Enable this
   manage_default_security_group = false
   manage_default_network_acl    = false
   manage_default_route_table    = false
