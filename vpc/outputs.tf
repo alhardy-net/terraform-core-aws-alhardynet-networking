cf1bad3d26aee87f792199fa5e683217c084fe2d
Allan Hardy <al_hardy@live.com.au>
2021-07-06 12:10:50 +1000

    adding subnet ids to vpc output

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,4 +1,19 @@
 output "vpc_id" {
   value       = module.aws-vpc.vpc_id
-  description = "The hub's VPC Identifier"
+  description = "The VPC Identifier"
+}
+
+output "public_subnets" {
+  value       = module.public-subnet.subnet_ids
+  description = "The Public Subnet Identifiers"
+}
+
+output "private_application_subnets" {
+  value       = module.private-application-subnet.subnet_ids
+  description = "The Private Application Subnet Identifiers"
+}
+
+output "private_persistence_subnets" {
+  value       = module.private-persistence-subnet.subnet_ids
+  description = "The Private Persistence Subnet Identifiers"
 }
