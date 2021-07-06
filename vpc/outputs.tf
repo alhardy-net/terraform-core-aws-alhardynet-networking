fbcc215ca402bbcd81ab88a5a7e489cd4d13312d
Allan Hardy <al_hardy@live.com.au>
2021-07-06 12:30:11 +1000

    remove subnet cidr blocks, module will gnerate more for each az, should output in module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -13,27 +13,12 @@ output "public_subnets" {
   description = "The Public Subnet Identifiers"
 }
 
-output "public_subnets_cidr_block" {
-  value       = local.public_subnet_cidr
-  description = "The CIDR block of the Public Subnet"
-}
-
 output "private_application_subnets" {
   value       = module.private-application-subnet.subnet_ids
   description = "The Private Application Subnet Identifiers"
 }
 
-output "private_application_subnets_cidr_block" {
-  value       = local.private_application_subnet_cidr
-  description = "The CIDR block of the Private Application Subnet"
-}
-
 output "private_persistence_subnets" {
   value       = module.private-persistence-subnet.subnet_ids
   description = "The Private Persistence Subnet Identifiers"
-}
-
-output "private_persistence_subnets_cidr_block" {
-  value       = local.private_persistence_subnet_cidr
-  description = "The CIDR block of the Private Persistence Subnet"
 }
