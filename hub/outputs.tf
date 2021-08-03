3f059de85f5a7d1b4ad7a81ba91c212ae2ef323e
Allan Hardy <al_hardy@live.com.au>
2021-08-03 10:03:35 +1000

    output hub cidr block

--- hub/outputs.tf
+++ hub/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value       = module.aws-vpc.vpc_id
   description = "The VPC Identifier"
 }
+
+output "vpc_cidr_block" {
+  value       = var.vpc_cidr
+  description = "The CIDR block of the VPC"
+}
